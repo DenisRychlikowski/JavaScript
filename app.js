e9eb065688dba5ad3379629a050a2446d6c982fc
unknown <drychlikowski@gmail.com>
2023-09-06 13:35:07 +0200

    Pierwsze zadania

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let name = "JavaScript"
+// document.write("<p>Imię: Denis</p><p>Nazwisko: Rychlikowski</p><p>email: drychlikowski@gmail.com</p><p>Numer telefonu: 781999273</p>")
 
-document.write("Hello " + name)
-document.write("<br />")
-document.write("Heeey")
+// document.write(`<img src="./l.jpg" alt="img">`)
+
+// alert("Denis Rychlikowski")
+
+// confirm("Jesteś pełnoletni?")
+
+let name = prompt("Podaj imie")
+let secondName = prompt("Podaj nazwisko")
+let email = prompt("Podaj emial")
+
+document.write("Nazwisko i imię: " + name + " " + secondName + "<br />")
+document.write("Adres e-mail: " + email)
