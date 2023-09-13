ec95542cfa0718f79655374562eb2b771b5c583f
unknown <drychlikowski@gmail.com>
2023-09-13 14:01:31 +0200

    Program na reszte pieniedzy

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let num1 = parseFloat(prompt("Podaj pierwsza liczbe"))
-let num2 = parseFloat(prompt("Podaj druga liczbe"))
+let money = prompt("Podaj hajs")
+let banknotyMonety = [ 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 ] 
 
-if(num2 == 0) {
-    alert("NIE DZIEL PRZEZ 0")
-} else {
-    document.write("Dzielenie: ", num1 / num2)
+document.write("Money: ", money, '<br />')
+
+for(let i = 0; i <= banknotyMonety.length; i++) {
+    let counter = Math.floor(money / banknotyMonety[i]) 
+    if(money >= banknotyMonety[i]){
+        document.write(counter," x ", banknotyMonety[i], "zł<br />")
+        money = money - (counter * banknotyMonety[i]) 
+        money = money.toFixed(2)
+    }
 }
